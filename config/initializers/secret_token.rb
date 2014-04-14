118ab8e73a4bde026035fe6840cd310bebfc06fc
Robert Moon <rkm_email@yahoo.com>
2014-04-14 10:46:05 -0700

    added chat

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Risk::Application.config.secret_key_base = '8e104b54bcda3dd32dbdec2be876687f8468a2d4b3015457644877597f1b0edb1f2658b7b1a7540b9d1db67625e4f58915408107a3a520fbcc75818039d98a30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Risk::Application.config.session_store :cookie_store, key: '_Risk_session'
