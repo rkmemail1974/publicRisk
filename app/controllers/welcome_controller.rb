7ea513fbf4f089f3f3fad3323ec2659328d0b4be
VirtualBoxImages.com <gphrost@gmail.com>
2014-04-23 19:47:15 -0400

    pusher demo added 5

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,9 @@
+# config/initializers/pusher.rb
+require 'pusher'
+
+Pusher.url = "http://e4cf2399c571ef4a7352:fb6c0be417b809658c5c@api.pusherapp.com/apps/72745"
+Pusher.logger = Rails.logger
+
 class WelcomeController < ApplicationController
   def hello
   Pusher['test_channel'].trigger('greet', {
