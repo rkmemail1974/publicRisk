9a2a603ee4c49574214e7eea993f8edd294578b5
VirtualBoxImages.com <gphrost@gmail.com>
2014-04-24 03:02:43 -0400

    Initial commit.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Risk::Application.routes.draw do
   get "welcome/hello"
+  post 'pusher/auth'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root :to => 'welcome#hello'
+   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
